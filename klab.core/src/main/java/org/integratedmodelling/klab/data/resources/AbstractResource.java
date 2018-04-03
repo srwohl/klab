eb1f046f1ce8ba8404d13b99cd23556fd95759ef
J. Luke Scott [klab] <luke@cron.works>
2018-04-03 11:47:50 +0200

    generate source jars during builds; removed some compiler warnings

--- klab.core/src/main/java/org/integratedmodelling/klab/data/resources/AbstractResource.java
+++ klab.core/src/main/java/org/integratedmodelling/klab/data/resources/AbstractResource.java
@@ -2,7 +2,7 @@ package org.integratedmodelling.klab.data.resources;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
+
 import org.integratedmodelling.klab.Version;
 import org.integratedmodelling.klab.api.data.IResource;
 import org.integratedmodelling.klab.api.knowledge.IMetadata;
@@ -63,5 +63,4 @@ public abstract class AbstractResource implements IResource {
     public List<INotification> getHistory() {
         return history;
     }
-    
 }
